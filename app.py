e5140dc051ab42f4244629f130e96ca721138d1c
solominh <hoangminhftu2@gmail.com>
2017-01-09 00:28:29 +0700

    Fix hotkey

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import pyautogui
 import win32clipboard
 import keyboard
-import time
+import datetime
 
 
 def read_clipboard():
@@ -22,6 +22,18 @@ def main():
     send_keys(data)
     print('Send keys successfully!')
 
+last_time_pressed = datetime.datetime.now()
+
+
+def handle_hotkey():
+    global last_time_pressed
+    now = datetime.datetime.now()
+    delta = now - last_time_pressed
+    if delta.total_seconds() > 3:
+        main()
+        last_time_pressed = now
+
+
 hotkey = keyboard.add_hotkey(
-    'ctrl+alt+shift+c,space', main, blocking=True)
+    'ctrl+alt+shift+k,Home', handle_hotkey, blocking=True, timeout=3)
 recorded = keyboard.record(until='esc')
